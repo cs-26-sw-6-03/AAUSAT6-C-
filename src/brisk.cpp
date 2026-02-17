10b262fede2274ac9ebea243662a5779a7a76d61
Naitsabot <naitsabot@gmail.com>
2026-02-17 09:20:26 +0100

    add file

---
+++ src/brisk.cpp
@@ -0,0 +1,17 @@
+// Brisk Cpp Class OpenCV
+// https://docs.opencv.org/4.x/de/dbf/classcv_1_1BRISK.html
+
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+ 
+#include <iostream>
+ 
+using namespace cv;
+
+int main(int argc, char *argv[]) {
+
+    
+
+    return 0;
+}
